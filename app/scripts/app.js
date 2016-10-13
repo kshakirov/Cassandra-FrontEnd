425b08d28bad9812658e2edefd6a1f4030d364e4
kshakirov <kshakirov@zoral.com.ua>
2016-10-13 12:47:16 +0300

    slider added working

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,28 +8,47 @@
  *
  * Main module of the application.
  */
-angular
-  .module('angularFrontendApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/home.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+// angular
+//   .module('angularFrontendApp', [
+//     'ngAnimate',
+//     'ngCookies',
+//     'ngResource',
+//     'ngRoute',
+//     'ngSanitize',
+//     'ngTouch'
+//   ])
+//   .config(function ($routeProvider) {
+//     $routeProvider
+//       .when('/', {
+//         templateUrl: 'views/home.html',
+//         controller: 'MainCtrl',
+//         controllerAs: 'main'
+//       })
+//       .when('/about', {
+//         templateUrl: 'views/about.html',
+//         controller: 'AboutCtrl',
+//         controllerAs: 'about'
+//       })
+//       .otherwise({
+//         redirectTo: '/'
+//       });
+//   });
+
+var magento_module = angular.module('MagentoApp', [ 'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch', 'angularSpinner','ngFader']);
+
+magento_module.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/home.html',
+      controller: 'HomeController',
+      controllerAs: 'main'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
