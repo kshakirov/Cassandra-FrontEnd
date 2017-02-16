ae79e88191f94236a89c0ee28b1b038ec212efa8
kshakirov <kshakirov@zoral.com.ua>
2017-02-16 14:49:09 +0200

    DONE #27 forgotten password added info

--- app/scripts/controllers/customer/forgotten_password.js
+++ app/scripts/controllers/customer/forgotten_password.js
@@ -2,14 +2,19 @@ magento_module.controller("ForgotPasswordController", function ($scope,
                                                                 $rootScope, $http,
                                                                 $timeout, $window) {
 
+  $scope.password_reset = false;
   $scope.resetPassword = function (email_address) {
     console.log("resetPassword");
     console.log(email_address);
     var payload = {email: email_address};
     $http.post("/frontend/customer/password/reset/", payload).then(function (promise) {
       console.log(promise);
+      $scope.password_reset_message = "Your Password Is Resetted Email with new credential sent to " + email_address;
+      $scope.password_reset = true;
     })
   }
+
+
 
   // $scope.reloadRoute = function () {
   //   console.log("set to reload");
