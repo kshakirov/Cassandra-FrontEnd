4e9847a85e6e798abd879dc4435f90f1023cef84
kshakirov <kshakirov@zoral.com.ua>
2017-06-09 18:26:50 +0300

    DONE #72 login vs account handling

--- app/scripts/controllers/customer/customer_login.js
+++ app/scripts/controllers/customer/customer_login.js
@@ -3,6 +3,11 @@ magento_module.controller("CustomerLogin", function ($scope,
                                                      $cookies, $location) {
 
   $scope.init = function () {
+    $http.get("/customer/account").then(function (promise) {
+      $location.path('/customer/account/')
+    }, function (error) {
+      console.log("Login page")
+    })
   };
 
   $scope.error = {
@@ -11,15 +16,13 @@ magento_module.controller("CustomerLogin", function ($scope,
   }
 
 
-
-
   function _submitPassword(data) {
     return $http.post("/frontend/customer/login", data)
       .then(function (promise) {
-          $cookies.putObject('token', promise.data.token);
-          $rootScope.$broadcast('loginDone', true);
-          $scope.isNotAuthorized = false;
-          $scope.error.flag = false;
+        $cookies.putObject('token', promise.data.token);
+        $rootScope.$broadcast('loginDone', true);
+        $scope.isNotAuthorized = false;
+        $scope.error.flag = false;
       }, function (error) {
         $scope.error.flag = true;
         $scope.error.msg = error.data.message;
@@ -28,9 +31,9 @@ magento_module.controller("CustomerLogin", function ($scope,
 
 
   $scope.submitPasswordByEnterClick = function (event, customer_email, password) {
-    if (event.which === 13){
+    if (event.which === 13) {
       var data = {
-        customer_email:customer_email,
+        customer_email: customer_email,
         password: password
       };
       return _submitPassword(data);
@@ -42,9 +45,8 @@ magento_module.controller("CustomerLogin", function ($scope,
       customer_email: $scope.customer_email,
       password: $scope.password
     };
-     return _submitPassword(data);
+    return _submitPassword(data);
   }
-
 
 
   $scope.createNewAccount = function () {
