92467198ce9b7872533bb36754702cc113688c8d
kshakirov <kshakirov@zoral.com.ua>
2017-06-16 16:52:56 +0300

    WIP #77 direct url bug 2

--- app/scripts/magento_application_run.js
+++ app/scripts/magento_application_run.js
@@ -1,34 +1,48 @@
 magento_module.run(function ($rootScope, $location) {
 
-  function _is_login_url(prevUrl) {
-    if (prevUrl == "/customer/account/login/")
-      return true
-    return false;
+    function _is_login_url(prevUrl) {
+      if (prevUrl == "/customer/account/login/")
+        return true
+      return false;
+
+    }
+
+    function _get_prev_url(history) {
+      if (history.length == 2) {
+        return history[0];
+      } else if (history.length > 2) {
+        return history.splice(-3)[0]
+      }
+
+      else {
+        return '/'
+      }
+    }
+
+    $rootScope.flags = {catalog: false};
+    var history = [];
+
+    $rootScope.$on('$routeChangeSuccess', function () {
+      if (history.length < 10) {
+        history.push($location.$$path);
+      } else {
+        history = history.splice(-4);
+        history.push($location.$$path);
+      }
+    });
+
+
+    $rootScope.back = function () {
+      var prevUrl = _get_prev_url(history);
+      if (_is_login_url(prevUrl)) {
+        $location.path("/");
+      } else {
+        $location.path(prevUrl);
+      }
+    };
+
+    $rootScope.elastic_index = 'turbointernational_development'
+
 
   }
-
-  $rootScope.flags = {catalog: false};
-  var history = [];
-
-  $rootScope.$on('$routeChangeSuccess', function () {
-    if (history.length < 10) {
-      history.push($location.$$path);
-    } else {
-      history = history.splice(-4);
-      history.push($location.$$path);
-    }
-  });
-
-  $rootScope.back = function () {
-    var prevUrl = history.length > 2 ? history.splice(-3)[0] : "/";
-    if (_is_login_url(prevUrl)) {
-      $location.path("/");
-    } else {
-      $location.path(prevUrl);
-    }
-  };
-
-  $rootScope.elastic_index = 'turbointernational_development'
-
-
-});
+);
