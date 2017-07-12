91310736f6e884a9ae02fa5a934aaee4ef121fbf
kshakirov <kshakirov@zoral.com.ua>
2017-07-12 17:32:45 +0300

    DONE #151 price scale

--- app/scripts/filters/cart_price.js
+++ app/scripts/filters/cart_price.js
@@ -1,16 +1,19 @@
 magento_module.filter('cart_price_filter', function ($cookies, $filter) {
 
 
-  function _get_current_currency(currency_code) {
+  function _get_current_currency(currency_code, scale) {
       var currencies = {'USD': '$',
       'GBP' : '£', "EUR" : "€"};
       return  currencies[currency_code];
   }
 
 
-  return function (price, currency_code) {
-
-    return  '<span class="price">' + $filter('currency')(price, _get_current_currency(currency_code), 2) + '</span>';
+  return function (price, currency_code, scale) {
+    var default_scale = 2;
+    if(scale != null){
+        default_scale = scale;
+    }
+    return  '<span class="price">' + $filter('currency')(price, _get_current_currency(currency_code), default_scale) + '</span>';
 
   };
 });
--- app/scripts/filters/product_price.js
+++ app/scripts/filters/product_price.js
@@ -17,12 +17,13 @@ magento_module.filter('product_price_filter', function ($cookies, $filter) {
 
   function product_price_filter(input, row) {
     var currency_object = _get_current_currency();
+    var precision = row || 2;
     var price = input;
     if (_is_not_default_currency(currency_object.code)) {
       price = _convert_to_other_currency(price, currency_object.code);
     }
 
-    return '<span class="price">' + $filter('currency')(price, currency_object.symbol, 2) + '</span>';
+    return '<span class="price">' + $filter('currency')(price, currency_object.symbol, precision) + '</span>';
 
   };
   product_price_filter.$stateful = true;
